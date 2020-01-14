059dbd3f16b0e230fa9fa90397f9f2c79381bfb5
Lucy <lucymidgley13@gmail.com>
2020-01-14 20:12:11 +0000

    Created spinner animation on command line with one full revolution

---
+++ spinner.js
@@ -0,0 +1,40 @@
+setTimeout(() => { 
+  process.stdout.write('\r| ');
+}, 100);
+
+setTimeout(() => { 
+  process.stdout.write('\r/ ');
+}, 300);
+
+setTimeout(() => { 
+  process.stdout.write('\r- ');
+}, 500);
+
+setTimeout(() => { 
+  process.stdout.write('\r\\ ');
+}, 700);
+
+setTimeout(() => { 
+  process.stdout.write('\r| ');
+}, 900);
+
+setTimeout(() => { 
+  process.stdout.write('\r/ ');
+}, 1100);
+
+setTimeout(() => { 
+  process.stdout.write('\r- ');
+}, 1400);
+
+setTimeout(() => { 
+  process.stdout.write('\r\\ ');
+}, 1600);
+
+setTimeout(() => { 
+  process.stdout.write('\r| ');
+}, 1800);
+
+setTimeout(() => { 
+  process.stdout.write('\n');
+}, 2000);
+
